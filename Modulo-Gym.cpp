a0f7381704f93178adc83164ed9eebae2569dccf
inakigarcia1 <inakigarciatarcic@gmail.com>
2023-01-31 12:29:15 -0300

    codigo comentado

--- Modulo-Gym.cpp
+++ Modulo-Gym.cpp
@@ -1,15 +1,75 @@
+/*
+
+							Algoritmos y Estructuras de Datos: TFI. Sistema de administración y gestión de gimnasio.
+
+
+ ________  ________  _____ ______   ___  ________  ___  ________  ________            _____  ___  __    ________     
+|\   ____\|\   __  \|\   _ \  _   \|\  \|\   ____\|\  \|\   __  \|\   ___  \         / __  \|\  \|\  \ |\_____  \    
+\ \  \___|\ \  \|\  \ \  \\\__\ \  \ \  \ \  \___|\ \  \ \  \|\  \ \  \\ \  \       |\/_|\  \ \  \/  /|\|____|\ /_   
+ \ \  \    \ \  \\\  \ \  \\|__| \  \ \  \ \_____  \ \  \ \  \\\  \ \  \\ \  \      \|/ \ \  \ \   ___  \    \|\  \  
+  \ \  \____\ \  \\\  \ \  \    \ \  \ \  \|____|\  \ \  \ \  \\\  \ \  \\ \  \          \ \  \ \  \\ \  \  __\_\  \ 
+   \ \_______\ \_______\ \__\    \ \__\ \__\____\_\  \ \__\ \_______\ \__\\ \__\          \ \__\ \__\\ \__\|\_______\
+    \|_______|\|_______|\|__|     \|__|\|__|\_________\|__|\|_______|\|__| \|__|           \|__|\|__| \|__|\|_______|
+                                           \|_________|   
+										   
+													 ___     ___    ___    ____   
+													|__ \   / _ \  |__ \  |___ \  
+													   ) | | | | |    ) |   __) | 
+													  / /  | | | |   / /   |__ <  
+													 / /_  | |_| |  / /_   ___) | 
+													|____|  \___/  |____| |____/  
+                             							   										                                                                                                                                                                        
+   
+   POR:     
+		                                                                                                            
+    ______      __   /\//               __  ___      __  __          			
+   /  _/ /_  __/_/__//\/ ___  ____     /  |/  /___ _/ /_/_/___ ______
+   / // __ \/ __ `/ __ \/ _ \/_  /    / /|_/ / __ `/ __/ / __ `/ ___/
+ _/ // /_/ / /_/ / / / /  __/ / /_   / /  / / /_/ / /_/ / /_/ (__  ) 
+/___/_.___/\__,_/_/ /_/\___/ /___/  /_/  /_/\__,_/\__/_/\__,_/____/  
+
+
+       __        __                     ______      __    __          
+      / /_  ____/_/_________  ____     /_  __/___  / /_  /_/___ ______
+ __  / / / / / __ `/ ___/ _ \/_  /      / / / __ \/ __ \/ / __ `/ ___/
+/ /_/ / /_/ / /_/ / /  /  __/ / /_     / / / /_/ / /_/ / / /_/ (__  ) 
+\____/\__,_/\__,_/_/   \___/ /___/    /_/  \____/_.___/_/\__,_/____/  
+
+    __  __                          __         _____             __  _           
+   / / / /___ _____ ___  ____ _____/ /___ _   / ___/____ _____  / /_(_)___  ____ 
+  / /_/ / __ `/ __ `__ \/ __ `/ __  / __ `/   \__ \/ __ `/ __ \/ __/ / __ \/ __ \
+ / __  / /_/ / / / / / / /_/ / /_/ / /_/ /   ___/ / /_/ / / / / /_/ / / / / /_/ /
+/_/ /_/\__,_/_/ /_/ /_/\__,_/\__,_/\__,_/   /____/\__,_/_/ /_/\__/_/_/ /_/\____/ 
+             
+			 
+   ______                __         ____ /\//       __   _ 
+  / ____/___ ___________/_/___ _   /  _///\/ ____ _/ /__(_)
+ / / __/ __ `/ ___/ ___/ / __ `/   / // __ \/ __ `/ //_/ / 
+/ /_/ / /_/ / /  / /__/ / /_/ /  _/ // / / / /_/ / ,< / /  
+\____/\__,_/_/   \___/_/\__,_/  /___/_/ /_/\__,_/_/|_/_/   
+                                                                                                                              
+                                                                                                                                     
+
+*/
+
 #include "datos.h"
 
 using namespace std;
 
+// Funciones
 void menu(int legajo);
 void LoginEntrenador(int &legajo);
 void Listado(int legajo);
 void Registrar(int legajo);
 
 main() {
+	
+	// Evitar estiramiento de la consola
 	corregirConsola();
+	
 	int legajo = 0;
+	
+	// Titulo
 	system("color 7");
 	cout<<"\t\t\t    __  __   __      _       _          _______     ____  __ \n";
 	Sleep(100);
@@ -24,16 +84,23 @@ main() {
 	cout<<"\t\t\t   |_|  |_|\\___/ \\__,_|\\__,_|_|\\___/   \\_____|  |_|  |_|  |_|\n";
 	Sleep(100);
 	cout<<"\n\n\n";
+	
+	// Se llama a login y despues al menu principal
 	LoginEntrenador(legajo);
 	menu(legajo);
 
 }
 
+// Funcion menu principal. Muestra nombre de usuario y opciones
 void menu(int legajo) {
 	int ope=0;
 	while(ope!=1 and ope!=4) {
 		system("cls");
+		
+		// Color azul
 		system("color 9");
+		
+		// Mostrar titulo y opciones de a poco
 		cout<<"\t\t\t    __  __   __      _       _          _______     ____  __ \n";
 		Sleep(100);
 		cout<<"\t\t\t   |  \\/  | /_/     | |     | |        / ____\\ \\   / /  \\/  |\n";
@@ -47,11 +114,15 @@ void menu(int legajo) {
 		cout<<"\t\t\t   |_|  |_|\\___/ \\__,_|\\__,_|_|\\___/   \\_____|  |_|  |_|  |_|\n";
 		Sleep(100);
 		cout<<"\n\n\n";
+		
+		// Mostrar nombre del entrenador segun legajo
 		if(legajo == 91218) cout<<setw(62)<<"MARCELO GALLARDO";
 		if(legajo == 24514) cout<<setw(62)<<"RAMON DIAZ";
 		if(legajo == 61122) cout<<setw(62)<<"MARTIN DEMICHELIS";
 		if(legajo == 26696) cout<<setw(62)<<"HERNAN CRESPO";
 		if(legajo == 65723) cout<<setw(62)<<"PABLO AIMAR";
+		
+		// Opciones del menu
 		Sleep(100);
 		cout<<"\n\n\n";
 		cout<<"\tElija una operacion"<<endl<<endl;
@@ -66,6 +137,8 @@ void menu(int legajo) {
 		Sleep(100);
 		cin>>ope;
 		printf("\n");
+		
+		// Bucle para seleccionar la opcion correcta
 		while(ope!=4 and ope!=0) {
 			switch(ope) {
 				case 1:
@@ -101,22 +174,23 @@ void menu(int legajo) {
 }
 
 void LoginEntrenador(int &legajo) {
+	
 	// Variables
 	Entrenador entrenador;
-
-//	int ch;
 	legajo = 0;
 	string contrasena;
 
 	bool correctUser = false;
 	bool correctPass = false;
 
-	FILE *arch = fopen("Entrenadores.dat","rb"); //abro el archivo
+	FILE *arch = fopen("Entrenadores.dat","rb"); // Abrir archivo
 
-	//EN PANTALLA//
-	cout<<setw(62)<<"INICIO DE SESION"<<endl;Sleep(1500);
+	// Datos en pantalla
+	cout<<setw(62)<<"INICIO DE SESION"<<endl;
+	Sleep(1500);
 	cout<<setw(62)<<"\nPor motivos de seguridad, su contrasena no sera mostrada mientras escribe.\n\n";
 
+	// Bucle para iniciar sesion
 	while(!correctUser or !correctPass) {
 
 		cout<<" - Legajo de entrenador: ";
@@ -133,16 +207,16 @@ void LoginEntrenador(int &legajo) {
 		//Leo del archivo y guardo en entrenador
 		fread(&entrenador, sizeof(Entrenador), 1, arch);
 
-		//Verifico:
+		//Verifico
 		while(!feof(arch)) {
 
 			correctUser = false;
 			correctPass = false;
 
-			if(legajo==entrenador.nroEntrenador) {
+			if(legajo==entrenador.nroEntrenador) { // Si el legajo coincide con uno del archivo, verifico su contraseña
 				correctUser = true;
 
-				if(contrasena == entrenador.contrasena) {
+				if(contrasena == entrenador.contrasena) { // Si la contraseña también coresponde, se rompe el bucle
 					correctPass = true;
 					break;
 				}
@@ -153,6 +227,7 @@ void LoginEntrenador(int &legajo) {
 
 		}
 
+		// Si no fueron encontrados el usuario y/o la contraseña, repito el bucle
 		if(!correctUser or !correctPass) {
 			printf("\n\n\tUsuario o contrasena incorrectos. Por favor, reintente.\n\n");
 			system("color 4");
@@ -170,6 +245,7 @@ void LoginEntrenador(int &legajo) {
 
 	cout<<"\n\nUsuario encontrado.\n";
 
+	// Mensaje de bienvenida al entrenador correspondiente
 	if(legajo == 91218) cout<<"Bienvenido, Marcelo. ";
 	if(legajo == 24514) cout<<"Bienvenido, Ramon. ";
 	if(legajo == 61122) cout<<"Bienvenido, Martin. ";
@@ -190,14 +266,19 @@ void LoginEntrenador(int &legajo) {
 }
 
 void Listado(int legajo) {
+	
+	// Varibales
 	Socio socio;
 	int c=1, op;
 	char act1[]="Zumba", act2[]="Pila", act3[]="Spinning";
+	
 	FILE *arch = fopen("Socios.dat", "r+b");
 	system("color 9");
 	cout<<"-------------Socios:-------------"<<endl<<endl;
+	
 	fread(&socio, sizeof(Socio), 1, arch);
 
+	// Se recorre el archivo para mostrar las actividades y sus horarios
 	while(!feof(arch)) {
 		cout<<" Socio numero "<<c<<":"<<endl<<endl;
 		cout<<" - Apellido y nombre: "<<socio.apeNom<<endl;
@@ -209,7 +290,7 @@ void Listado(int legajo) {
 				for(int k=0; k<3; k++) {
 					if(socio.actividadYTurno[i][j][k]==1) {
 						cout<<"\t* ";
-						//ACTIVIDAD
+						// Comprobar actividad
 						if(k==0) {
 							cout<<"Zumba -";
 						}
@@ -219,7 +300,7 @@ void Listado(int legajo) {
 						if(k==2) {
 							cout<<"Pilates -";
 						}
-						//DIA
+						// Comprobar dia
 						switch(j) {
 							case 0:
 								cout<<" Los lunes";
@@ -242,7 +323,7 @@ void Listado(int legajo) {
 							default:
 								break;
 						}
-						//HORARIO
+						// Horario
 						if(i==0) {
 							cout<<" - 1er turno (8hs - 16hs).";
 						} else {
@@ -269,7 +350,11 @@ void Registrar(int legajo) {
 	system("color 9");
 
 	FILE *arch = fopen("Socios.dat", "rb");
-	int numsocio, sp=32, words;
+	
+	// Variables
+	int numsocio = 0;
+	int sp = 32; // Codigo ASCII del caracter "espacio"
+	int words = 0;
 	char rutina[10000];
 	Socio socio;
 	int letra = 0;
@@ -284,9 +369,10 @@ void Registrar(int legajo) {
 
 	fread(&socio, sizeof(Socio), 1, arch);
 
+	// Recorro el archivo
 	while(!feof(arch)) {
 
-		if(numsocio==socio.nroSocio) {
+		if(numsocio==socio.nroSocio) { // Verifico numero de socio
 			system("color 9");
 			cout<<"\tSocio encontrado."<<endl;
 			system("color 2");
@@ -296,27 +382,31 @@ void Registrar(int legajo) {
 			system("color 2");
 			Sleep(500);
 			system("color 9");
-			encontrado = true;
-			break;
+			encontrado = true; // Socio encontrado
+			break; // Salir del bucle
 		} else fread(&socio, sizeof(Socio), 1, arch);
 
 	}
 	rewind(arch);
 
+	// Si no se encontro el nroSocio:
 	if(!encontrado) {
 		system("color 9");
 		cout<<"Datos de socio incorrectos. ";
 		error();
 		cout<<"Presione una tecla para volver al menu...";
 		getch();
-		menu(legajo);
+		menu(legajo); // Volver al menu
 	}
 
 	system("color 9");
 	cout<<"\n\tPor favor, ingresar por teclado una rutina de gimnasia, recordar que la misma no debe superar las 380 palabras."<<endl<<endl;
 	cout<<"\tRutina: ";
+	
+	// Leer rutina
 	_flushall();
 	gets(rutina);
+	
 	printf("\n\n");
 
 	words=0;
@@ -331,13 +421,15 @@ void Registrar(int legajo) {
 	Sleep(800);
 	printf("\n\n");
 
+	// Recorro la rutina caracter por caracter para comprobar que no sobrepase las 380 palabras
 	for(int i = 0; i < strlen(rutina); i++) {
 
-		letra = rutina[i];
+		letra = rutina[i]; // "Letra" almacena el codigo ASCII del caracter en la posicion "i" de la rutina
 
-		if(letra == sp) words++;
+		if(letra == sp) words++; // Si el codigo ASCII es igual al del caracter de espacio en blanco, entonces aumenta en 1 la cantidad de palabras
 	}
 
+	// Si se cumple la restriccion:
 	if(words<=380) {
 		system("color 9");
 		cout<<"\tLa rutina ingresada cumple con las restricciones."<<endl;
@@ -351,33 +443,24 @@ void Registrar(int legajo) {
 		printf(".");
 		Sleep(800);
 
-		//Subiendo al archivo
+		// Subiendo al archivo
 		strcpy(socio.rutina, rutina);
 		fwrite(&socio, sizeof(Socio), 1, arch);
 
 		printf("\n\n");
 		cout<<"Datos subidos correctamente. Presione una tecla para volver al menu...";
 		getch();
-	}
-	else {
+		
+	// Si no se cumple:
+	} else {
 		system("color 9");
 		cout<<"\tLa rutina detallada no cumple con los requisitos de extension. Por favor, ingrese una rutina valida."<<endl<<endl;
 		error();
 		cout<<"\tRutina: ";
 		_flushall();
-		gets(rutina);
+		gets(rutina); // Leo nuevamente la rutina
 		printf("\n\n");
 	}
 	fclose(arch);
 	menu(legajo);
 }
-
-
-
-
-
-
-
-
-
-
