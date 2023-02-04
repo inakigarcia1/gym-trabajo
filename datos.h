e9b8e35a5cda7bc06dfebeb984f378e21c575c3d
inakigarcia1 <inakigarciatarcic@gmail.com>
2023-02-04 16:30:30 -0300

    quitadas librerias inutilizadas

--- datos.h
+++ datos.h
@@ -7,19 +7,6 @@
 #include <conio.h>
 #define _WIN32_WINNT 0x0500
 #include <windows.h>
-#pragma once
-#include <string>
-
-// define fixed size integer types
-#ifdef _MSC_VER
-// Windows
-typedef unsigned __int8  uint8_t;
-typedef unsigned __int32 uint32_t;
-typedef unsigned __int64 uint64_t;
-#else
-// GCC
-#include <stdint.h>
-#endif
 
 using namespace std;
 
